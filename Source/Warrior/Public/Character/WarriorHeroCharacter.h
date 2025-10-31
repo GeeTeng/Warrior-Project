3987a21c7c45f51f0835289a3727d222c231cff7
GeeTeng <15143483@qq.com>
2025-10-31 19:51:54 +0800

    camera and debugHelper

---
+++ Source/Warrior/Public/Character/WarriorHeroCharacter.h
@@ -0,0 +1,33 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Character/WarriorBaseCharacter.h"
+#include "WarriorHeroCharacter.generated.h"
+
+class USpringArmComponent;
+class UCameraComponent;
+/**
+ * 
+ */
+UCLASS()
+class WARRIOR_API AWarriorHeroCharacter : public AWarriorBaseCharacter
+{
+	GENERATED_BODY()
+public:
+	AWarriorHeroCharacter();
+	
+protected:
+	virtual void BeginPlay() override;
+
+private:
+#pragma region Components:
+	UPROPERTY(visibleAnywhere, BlueprintReadOnly, Category = Camera, meta = (AllowPrivateAccess = "true"))
+	USpringArmComponent* CameraBoom;
+	UPROPERTY(visibleAnywhere, BlueprintReadOnly, Category = Camera, meta = (AllowPrivateAccess = "true"))
+	UCameraComponent* FollowCamera;
+	
+#pragma endregion
+	
+};
