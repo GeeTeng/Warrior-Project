fa4e38e6b13ed7ebb5c3c49224d2add13efc1f98
GeeTeng <15143483@qq.com>
2025-10-31 21:34:39 +0800

    data asset input config and gameplay tag

---
+++ Source/Warrior/Private/DataAssets/Input/DataAsset_InputConfig.cpp
@@ -0,0 +1,18 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "DataAssets/Input/DataAsset_InputConfig.h"
+
+
+// 查找匹配标签的动作
+UInputAction* UDataAsset_InputConfig::FindNativeInputActionByTag(const FGameplayTag& InInputTag)
+{
+	for (const FWarriorInputActionConfig& InputActionConfig : NativeInputActions)
+	{
+		if (InputActionConfig.InputTag == InInputTag && InputActionConfig.InputAction)
+		{
+			return InputActionConfig.InputAction;
+		}
+	}
+	return nullptr;
+}
